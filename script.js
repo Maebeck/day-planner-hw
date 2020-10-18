9353eb4a8627ab365e01ea7d3ce094bb12117b46
Corey Smith <CSSmith1997@gmail.com>
2020-10-17 18:36:58 -0600

    Wrote the JS using moment.js to display current time down to the second

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-//var storageInput = document.querySelector('.time-block');
-//var text = document.querySelector('.text');
-//var saveBtn = document.querySelector('.saveBtn');
-//var storedInput = localStorage.getItem('textinput');
- 
-$('.time-block').each(function(index){
+
+var timeofDay = [ '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
+
+var clock = document.getElementById("currentDay");
+setInterval(() => {
+    var now = moment();
+    var currentTime = now.format('MMMM Do YYYY, h:mm:ss A');
+    $('#currentDay').text(currentTime);
+}, 1000);
+
+
+$('.time-block').each(function(){
 var saveID = $(this).attr("id");
 $(this).val(localStorage.getItem(saveID));
 })
